22c45eb6aa858f01e7d23ab2db5d17486ab4e337
NinManj <119310712+NinManj@users.noreply.github.com>
2022-12-01 04:29:16 +0300

    Add files via upload

---
+++ func.py
@@ -0,0 +1,46 @@
+import subprocess
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository.GdkPixbuf import Pixbuf
+import os
+
+def update(store):
+    store.clear()
+    buff = ''
+    i = 0
+    bashc = str(subprocess.check_output('flatpak list --columns=name,application', shell=True).decode())
+    for c in bashc:
+        buff += str(c)
+        if c == "\t":
+            iter = store.append([buff, 'b'])
+            buff = ''
+        if c == "\n":
+            buff = buff[:-1]
+            store[iter][1] = buff
+            buff = ''
+
+def imgupdate(todel,appimg):
+    if len(todel) == 1:
+        if os.path.exists("/var/lib/flatpak/exports/share/icons/hicolor/scalable/apps/"+todel[0]+".svg"):
+            pixbuf = Pixbuf.new_from_file("/var/lib/flatpak/exports/share/icons/hicolor/scalable/apps/"+todel[0]+".svg")
+            appimg.set_from_pixbuf(pixbuf)
+        else:
+            appimg.set_from_pixbuf()
+    else:
+        appimg.set_from_pixbuf()
+
+def message(inf,Handler,Text):
+    inf.set_border_width(20)  # размер рамки окна
+    inf.set_decorated(0)
+    label = Gtk.Label()
+    label.set_markup(Text)
+    label.set_justify(Gtk.Justification.CENTER)
+    box = Gtk.Grid()
+    box.attach(label, 1, 0, 2, 1)
+    btn = Gtk.Button('Close')
+    btn.connect('clicked', Handler.infoQuit)
+    box.attach(btn, 1, 2, 2, 1)
+    inf.set_title("Info")
+    inf.add(box)
+    inf.show_all()
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import gi
 import os
-import subprocess
+import func
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from gi.repository.GdkPixbuf import Pixbuf
 
 inf=Gtk.Window() #Окно с информацией
 todel=[] #Список для удаления
@@ -17,17 +16,16 @@ class Handler:
     def clear(self,button): # (clicked для updatebutton) Выход из программы
         store.clear()
     def appear(self,*args): #(visibility-notify-event) Загрузка списка программ
-        update()
+        namebuffer = appname.get_buffer()
+        namebuffer.set_text("Choose an app...")
+        func.update(store)
     def sel(self,*args): # (cursor-changed и button-release-event) Обработчик выбора комманд
         todel.clear()
         textbuffer = appprop.get_buffer()
+        namebuffer = appname.get_buffer()
         select = applist.get_selection()
         Gtk.TreeSelection.set_mode(select, Gtk.SelectionMode.MULTIPLE)
         model, treeiter = select.get_selected_rows()
-        global ti
-        global mod
-        ti=treeiter
-        mod=model
         i=0
         print("Выбрано: ")
         while (i<len(treeiter)):
@@ -35,12 +33,12 @@ class Handler:
             i=i+1
         print(todel)
         if len(todel)==1:
-            appname.set_markup(model[treeiter][0])
-            imgupdate()
+            namebuffer.set_text(model[treeiter][0])
+            func.imgupdate(todel,appimg)
         else:
             if len(todel)!=0:
-                appname.set_markup("Multiple apps are selected:")
-                imgupdate()
+                namebuffer.set_text("Multiple apps are selected:")
+                func.imgupdate(todel,appimg)
         if treeiter is not None:
             if len(todel)==1:
                 textbuffer.set_text("App ID: " + todel[0] + "\nType: Flatpak")
@@ -53,51 +51,14 @@ class Handler:
                     i=i+1
     def dir(self, *args):
         if len(todel)==1:
-            os.system('xdg-open ~/.var/app/'+mod[ti][1])
+            os.system('xdg-open ~/.var/app/'+todel[0])
 
     def delete(deletion,*args):#(clicked для deletebutton) Удаление программ
         for app in todel:
             os.system('flatpak uninstall -y '+ app)
-        update()
+        func.update(store)
     def info(self,btn):#(clicked для infobutton) Формирование окна с информацией
-        global inf
-        inf.set_border_width(20)  # размер рамки окна
-        inf.set_decorated(0)
-        label=Gtk.Label()
-        label.set_markup("PHLÆTPÆK DELETER\nv.0.3_py\nconvinient tool to manage your Flatpaks\nNinMan(c) - 2022")
-        label.set_justify(Gtk.Justification.CENTER)
-        box = Gtk.Grid()
-        box.attach(label,1,0,2,1)
-        btn=Gtk.Button('Close')
-        btn.connect('clicked',self.infoQuit)
-        box.attach(btn, 1, 2, 2 ,1)
-        inf.set_title("Info")
-        inf.add(box)
-        inf.show_all()
-#Подпрограммки нужные внеклассовой части программы====================================
-def update(): #Обновление списка программ
-    store.clear()
-    buff = ''
-    i = 0
-    bashc = str(subprocess.check_output('flatpak list --columns=name,application', shell=True).decode())
-    for c in bashc:
-        buff += str(c)
-        if c == "\t":
-            iter = store.append([buff, 'b'])
-            buff = ''
-        if c == "\n":
-            buff = buff[:-1]
-            store[iter][1] = buff
-            buff = ''
-def imgupdate(): #Обновление списка программ
-    if len(todel) == 1:
-        if os.path.exists("/var/lib/flatpak/exports/share/icons/hicolor/scalable/apps/"+todel[0]+".svg"):
-            pixbuf = Pixbuf.new_from_file("/var/lib/flatpak/exports/share/icons/hicolor/scalable/apps/"+todel[0]+".svg")
-            appimg.set_from_pixbuf(pixbuf)
-        else:
-            appimg.set_from_pixbuf()
-    else:
-        appimg.set_from_pixbuf()
+        func.message(inf,Handler,"PHLÆTPÆK DELETER\nv.0.3_py\nconvinient tool to manage your Flatpaks\nNinMan(c) - 2022")
 #Основная часть программы===========================================================
 #Подключаемся к Глайду==============================================================
 builder = Gtk.Builder()
@@ -115,6 +76,8 @@ appprop=builder.get_object("appprop")
 appprop.set_size_request(200,300) #размер виджета списка
 appimg=builder.get_object("appimg")
 appname=builder.get_object("appname")
+delbutton=builder.get_object("delbutton")
+delbutton.get_style_context().add_class(Gtk.STYLE_CLASS_DESTRUCTIVE_ACTION)
 
 #Настраиваем отображение контента в ListView=========================================
 store = Gtk.ListStore(str,str) #Инициализируем хранилище для списка
